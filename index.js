ac02fc6db3f18ceca21f5d54fd1fca3a85d0d032
McKendree Strommer <mckendree.strommer@gmail.com>
2021-09-01 10:48:55 -0700

    added other drums - funcionality all delayed just a touch, difficult to play

--- index.js
+++ index.js
@@ -3,9 +3,13 @@
 
 const sounds = {
     'Space': './assets/audio/Bass-Drum-1.wav',
+    'KeyJ': './assets/audio/Closed-Hi-Hat-1.wav',
+    'KeyK': './assets/audio/Wet-Open-Hi-Hat-1.wav',
+    'KeyH': './assets/audio/Snare-Drum-1.wav'
 }
 
 const playSound = (event) => {
+    console.log(event.code);
     const sound = new Audio(sounds[event.code]);
     sound.play();
 }
