8f6d5ba59290e599f863727303babeeac6f70910
McKendree Strommer <mckendree.strommer@gmail.com>
2021-09-04 18:14:19 -0700

    added open hat, closed hat, timeouts for front end "animations"

--- public/index.js
+++ public/index.js
@@ -1,7 +1,7 @@
 const AudioContext = window.AudioContext || window.webkitAudioContext;
 let audioCtx;
 
-const init = () => {
+const init = async () => {
   audioCtx = new AudioContext();
   const buffer = audioCtx.createBuffer(
     1,
@@ -15,7 +15,7 @@ const init = () => {
   }
 
   const primaryGainControl = audioCtx.createGain();
-  primaryGainControl.gain.setValueAtTime(0.05, 0);
+  primaryGainControl.gain.setValueAtTime(0.5, 0);
   primaryGainControl.connect(audioCtx.destination);
 
   const snareFilter = audioCtx.createBiquadFilter();
@@ -23,25 +23,115 @@ const init = () => {
   snareFilter.frequency.value = 2000; // Measured in Hz
   snareFilter.connect(primaryGainControl);
 
+  const closedHatResponse = await fetch(
+    'https://unpkg.com/@teropa/drumkit@1.1.0/src/assets/hatClosed2.mp3'
+  ).catch((err) => {
+    console.log(err);
+  });
+  const closedBuffer = await closedHatResponse.arrayBuffer();
+  const closedHatBuffer = await audioCtx.decodeAudioData(closedBuffer);
+
+  const openHatResponse = await fetch(
+    'https://unpkg.com/@teropa/drumkit@1.1.0/src/assets/hatOpen2.mp3'
+  ).catch((err) => {
+    console.log(err);
+  });
+  const openBuffer = await openHatResponse.arrayBuffer();
+  const openHatBuffer = await audioCtx.decodeAudioData(openBuffer);
+
   const playSnare = () => {
+    document.querySelector('#snare_drum').style.background = 'white';
+    document.querySelector('#snare_drum').style.border = '1px solid black';
+    setTimeout(() => {
+      document.querySelector('#snare_drum').style.background = 'black';
+      document.querySelector('#snare_drum').style.border = '';
+    }, 500);
     const whiteNoiseSource = audioCtx.createBufferSource();
     whiteNoiseSource.buffer = buffer;
-    whiteNoiseSource.connect(snareFilter);
+    const whiteNoiseGain = audioCtx.createGain();
+    whiteNoiseGain.gain.setValueAtTime(1, audioCtx.currentTime);
+    whiteNoiseGain.gain.exponentialRampToValueAtTime(
+      0.01,
+      audioCtx.currentTime + 0.2
+    );
+    whiteNoiseSource.connect(whiteNoiseGain);
+    whiteNoiseGain.connect(snareFilter);
+
     whiteNoiseSource.start();
+    whiteNoiseSource.stop(audioCtx.currentTime + 0.2);
+
+    const snareOscillator = audioCtx.createOscillator();
+    snareOscillator.type = 'triangle';
+    snareOscillator.frequency.setValueAtTime(100, 0);
+
+    const snareOscillatorGain = audioCtx.createGain();
+    snareOscillatorGain.gain.setValueAtTime(0.2, audioCtx.currentTime);
+    snareOscillatorGain.gain.exponentialRampToValueAtTime(
+      0.1,
+      audioCtx.currentTime + 0.2
+    );
+    snareOscillator.connect(snareOscillatorGain);
+    snareOscillatorGain.connect(primaryGainControl);
+    snareOscillator.start();
+    snareOscillator.stop(audioCtx.currentTime + 0.2);
   };
 
   const playKick = () => {
-      const kickOscillator = audioCtx.createOscillator();
-      kickOscillator.frequency.setValueAtTime(261.1, 0);
-      kickOscillator.connect(primaryGainControl);
-      kickOscillator.start();
-      kickOscillator.stop(audioCtx.currentTime + 0.5);
-  }
+    document.querySelector('#bass_drum').style.background = 'white';
+    document.querySelector('#bass_drum').style.border = '1px solid black';
+    setTimeout(() => {
+      document.querySelector('#bass_drum').style.background = 'black';
+      document.querySelector('#bass_drum').style.border = '';
+    }, 500);
+    const kickOscillator = audioCtx.createOscillator();
+    kickOscillator.frequency.setValueAtTime(150, audioCtx.currentTime);
+    kickOscillator.frequency.exponentialRampToValueAtTime(
+      0.01,
+      audioCtx.currentTime + 0.5
+    );
+
+    const kickGain = audioCtx.createGain();
+    kickGain.gain.setValueAtTime(1, 0);
+    kickGain.gain.exponentialRampToValueAtTime(
+      0.01,
+      audioCtx.currentTime + 0.5
+    );
+
+    kickOscillator.connect(kickGain);
+    kickGain.connect(primaryGainControl);
+    kickOscillator.start();
+    kickOscillator.stop(audioCtx.currentTime + 0.5);
+  };
+
+  const playClosedHat = async () => {
+    document.querySelector('#closed_hat').style.background = 'white';
+    setTimeout(() => {
+      document.querySelector('#closed_hat').style.background = 'black';
+    }, 450);
+    const closedHatSource = audioCtx.createBufferSource();
+    closedHatSource.buffer = closedHatBuffer;
+    closedHatSource.connect(primaryGainControl);
+    closedHatSource.start();
+  };
+
+  const playOpenHat = async () => {
+    document.querySelector('#open_hat').style.background = 'white';
+    setTimeout(() => {
+      document.querySelector('#open_hat').style.background = 'black';
+    }, 450);
+    const openHatSource = audioCtx.createBufferSource();
+    openHatSource.buffer = openHatBuffer;
+    openHatSource.connect(primaryGainControl);
+    openHatSource.start();
+  };
 
   const sounds = {
     Space: playKick,
-    // 'KeyJ': closedHatEl,
-    // 'KeyK': openHatEl,
+    KeyJ: playClosedHat,
+    KeyK: playOpenHat,
+    KeyF: playClosedHat,
+    KeyD: playOpenHat,
+    KeyG: playSnare,
     KeyH: playSnare,
   };
 
@@ -61,4 +151,22 @@ document.querySelector('#start').addEventListener('click', () => {
   if (audioCtx.state === 'suspended') {
     audioCtx.resume();
   }
+
+  document
+    .querySelector('#center_column')
+    .removeChild(document.querySelector('#start'));
+
+  const bassDrumEl = document.createElement('div');
+  bassDrumEl.id = 'bass_drum';
+  const snareDrumEl = document.createElement('div');
+  snareDrumEl.id = 'snare_drum';
+  const closedHatEl = document.createElement('div');
+  closedHatEl.id = 'closed_hat';
+  const openHatEl = document.createElement('div');
+  openHatEl.id = 'open_hat';
+
+  document.querySelector('#center_column').appendChild(snareDrumEl);
+  document.querySelector('#center_column').appendChild(bassDrumEl);
+  document.querySelector('#center_column').appendChild(closedHatEl);
+  document.querySelector('#center_column').appendChild(openHatEl);
 });
